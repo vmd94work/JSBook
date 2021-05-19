d0a267ad3d03ceae87abb545dff738275d49b048
Vitalii <vmd94.work@gmail.com>
2021-05-19 22:38:20 +0300

    function decoration

--- testJS.js
+++ testJS.js
@@ -926,3 +926,23 @@ function spy(func) {
   return wrapper;
 }
  */
+
+/* function delay(f, ms) {
+  return function () {
+    setTimeout(() => f.apply(this, arguments), ms);
+  };
+}
+let f1000 = delay(alert, 1000);
+let f1500 = delay(alert, 1500);
+f1000("test"); // показывает "test" после 1000 мс
+f1500("test");
+ */
+function debounce(f, ms) {
+  let coolDown = false;
+  return function () {
+    if (coolDown) return;
+    f.apply(this, arguments);
+    coolDown = true;
+    setTimeout(() => (coolDown = false), ms);
+  };
+}
